ecbed3e2bbc296839512790265c639fdcc19df24
Harsha <95213910+Devilharsha@users.noreply.github.com>
2022-02-09 16:30:47 +0530

    Update bancodes.py

--- Skynet_System/plugins/bancodes.py
+++ Skynet_System/plugins/bancodes.py
@@ -1,26 +1,26 @@
    
 help_plus = """ Here is Help for **Bancodes** :-
-**❂** `{SXSx00}` **SCAMMER.**
-**❂** `{SXSx01}` **SPAM ADDING MEMBER.**
-**❂** `{SXSx03}` **ABUSE SPAM.**
-**❂** `{SXSx04}` **NSFW SPAMMER.**
-**❂** `{SXSx06}` **IMPERSONATION.**
-**❂** `{SXSx07}` **MD/BTC SCAM.**
-**❂** `{SXSx08}` **ADDING SPAMBOTS.**
-**❂** `{SXSx10}` **ILLEGAL.**
-**❂** `{SXSx11}` **PHISHING.**
-**❂** `{SXSx12}` **FRAUD PROMOTION  [ANY KIND].**
-**❂** `{SXSx13}` **CYBER THREATENING / CYBER BULLY.**
-**❂** `{SXSx14}` **CHILD ABUSE.**
-**❂** `{SXSx15}` **BAN EVASION.**
-**❂** `{SXSx16}` **SPAMBOT.**
-**❂** `{SXSx17}` **RAID INITIALIZOR.** 
-**❂** `{SXSx18}` **RAID PARTICIPANT.**
-**❂** `{SXSx19}` **KRIMINALANT.**
-**❂** `{SXSx20}` **SPAMMING VIOLENT CONTENT.**
-**❂** `{SXSx21}` **HATE SPEECH AGAINST NATIONALITY.**
-**❂** `{SXSx22}` **RAID/SPAM INFLAMMER.**
-**❂** `{SXSx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
-**❂** `{SXSx24}` **PAID GIRL.**
+**❂** `{SXEx00}` **SCAMMER.**
+**❂** `{SXEx01}` **SPAM ADDING MEMBER.**
+**❂** `{SXEx03}` **ABUSE SPAM.**
+**❂** `{SXEx04}` **NSFW SPAMMER.**
+**❂** `{SXEx06}` **IMPERSONATION.**
+**❂** `{SXEx07}` **MD/BTC SCAM.**
+**❂** `{SXEx08}` **ADDING SPAMBOTS.**
+**❂** `{SXEx10}` **ILLEGAL.**
+**❂** `{SXEx11}` **PHISHING.**
+**❂** `{SXEx12}` **FRAUD PROMOTION  [ANY KIND].**
+**❂** `{SXEx13}` **CYBER THREATENING / CYBER BULLY.**
+**❂** `{SXEx14}` **CHILD ABUSE.**
+**❂** `{SXEx15}` **BAN EVASION.**
+**❂** `{SXEx16}` **SPAMBOT.**
+**❂** `{SXEx17}` **RAID INITIALIZOR.** 
+**❂** `{SXEx18}` **RAID PARTICIPANT.**
+**❂** `{SXEx19}` **KRIMINALANT.**
+**❂** `{SXEx20}` **SPAMMING VIOLENT CONTENT.**
+**❂** `{SXEx21}` **HATE SPEECH AGAINST NATIONALITY.**
+**❂** `{SXEx22}` **RAID/SPAM INFLAMMER.**
+**❂** `{SXEx23}` **PRONOGRAPHY CCONTENT PROMOTING.**
+**❂** `{SXEx24}` **PAID GIRL.**
 """
 __plugin_name__ = "bancodes"
